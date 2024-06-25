66fefb67b71ac6858ae67bf6d500e588bcd0fdb1
fffk10 <warafffk10@gmail.com>
2024-06-25 22:04:54 +0900

    setup biome

--- src/app/providers/base-provider.tsx
+++ src/app/providers/base-provider.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react'
 import { UIProvider } from '@yamada-ui/react'
+import type { ReactNode } from 'react'
 
 export default function BaseProvider({ children }: { children: ReactNode }) {
   return <UIProvider>{children}</UIProvider>
